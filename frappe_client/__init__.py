2a8db25f562ed81041e294e869a05a64e1119d17
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2020-06-18 09:53:18 -0500

    Some Black on the files.

--- frappe_client/__init__.py
+++ frappe_client/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Frappe Client."""
 
 __author__ = """Sebastian Reyes Espinosa"""
-__email__ = 'sebaslander@gmail.com'
-__version__ = '2.0.0'
+__email__ = "sebaslander@gmail.com"
+__version__ = "2.0.0"
--- frappe_client/cli.py
+++ frappe_client/cli.py
@@ -6,8 +6,9 @@ import click
 @click.command()
 def main(args=None):
     """Console script for frappe_client."""
-    click.echo("Replace this message by putting your code into "
-               "frappe_client.cli.main")
+    click.echo(
+        "Replace this message by putting your code into " "frappe_client.cli.main"
+    )
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
