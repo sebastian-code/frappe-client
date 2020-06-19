e094be3fbc9b0806b4ee6e866bd64c0323956947
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2020-06-19 16:12:58 -0500

    The basic connector object.

--- frappe_client/frappe_client.py
+++ frappe_client/frappe_client.py
@@ -1,1 +1,26 @@
-"""Main module."""
+"""Frappe client core module."""
+
+from base64 import b64encode
+
+from decouple import config
+import requests
+
+
+class FrappeClient:
+    API_KEY = config("API_KEY")
+    API_SECRET = config("API_SECRET")
+    token = str(b64encode(f"{API_KEY}:{API_SECRET}".encode("utf-8")), "utf-8")
+    headers = {
+        "Authorization": f"Basic {token}",
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+    }
+
+    def __init__(self, verify=True, url=None):
+        self.verify = verify
+        self.session = requests.Session()
+        self.session.headers = headers
+        self.url = url
+
+    def __build_url__(self, doctype):
+        return f"{self.url}/api/resource/{doctype}"
